10c92f79edac14980ab5fc9ea3fcd1981d4974f5
Toqa <tokamohsen@outlook.com>
2019-12-04 20:07:54 +0200

    update add asset design

--- Brokery/ViewControllers/AddAssetViewController.swift
+++ Brokery/ViewControllers/AddAssetViewController.swift
@@ -14,9 +14,20 @@ class AddAssetViewController: BaseViewController {
     @IBOutlet var assetNameTextField: UITextField!
     @IBOutlet var assetDescriptionTextField: UITextField!
     @IBOutlet var hashtagTextField: UITextField!
+    @IBOutlet var assetTypeDropdownTextField: DropDown!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+//        //// The list of array to display. Can be changed dynamically
+//        dropDown.optionArray = ["Option 1", "Option 2", "Option 3"]
+//        //Its Id Values and its optional
+//        dropDown.optionIds = [1,23,54,22]
+//
+//        // Image Array its optional
+//        dropDown.ImageArray = [👩🏻‍🦳,🙊,🥞]
+//        // The the Closure returns Selected Index and String
+//        dropDown.didSelect{(selectedText , index ,id) in
+//            self.valueLabel.text = "Selected String: \(selectedText) \n index: \(index)"
+//        }
         // Do any additional setup after loading the view.
     }
     
